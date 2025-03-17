4aec02de83da5342b79477612cc21179393e0620
balachandarg-tw <balachandar.g@thoughtworks.com>
2025-03-17 16:09:54 +0530

    [INJIMOB-2425]: Remove Response mapping to data class , update it to generic JsonElement Type (#49)
    * [INJIMOB-2425]: Remove Response mapping to data class , update it to generic JsonElement Type
    
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>
    
    * [INJIMOB-2425]: Use single class for Credntial Response.
    
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>
    
    * [INJIMOB-2425]: Add test for mdoc Credential Response
    
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>
    
    * [INJIMOB-2425]: Update patch version
    
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>
    
    * [INJIMOB-3055]: Update the artifact version to Minor Version
    
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>
    
    ---------
    
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>

--- kotlin/vci-client/src/main/java/io/mosip/vciclient/credentialResponse/CredentialResponse.kt
+++ kotlin/vci-client/src/main/java/io/mosip/vciclient/credentialResponse/CredentialResponse.kt
@@ -1,5 +1,12 @@
 package io.mosip.vciclient.credentialResponse
 
-interface CredentialResponse {
-    fun toJsonString(): String
+import com.google.gson.JsonElement
+import io.mosip.vciclient.common.JsonUtils
+
+data class CredentialResponse(
+    val credential: JsonElement,
+)  {
+    fun toJsonString(): String {
+        return JsonUtils.serialize(this)
+    }
 }
